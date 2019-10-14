14fb16d1494ffeffec3aef28a03f3f4b9e1207b9
Soyu <sobalikdw@gmail.com>
2019-10-14 09:20:08 +0200

    2 commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Zmieniono plik main!");
+        System.out.println("Hi I'm Cloned!");
     }
 }
